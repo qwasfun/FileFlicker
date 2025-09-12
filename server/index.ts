747ac2a82a67b314e06754da6fe9a06c060e9629
qwas <git@qwas.fun>
2025-09-12 18:08:05 +0800

    chore: Increase log output

--- server/index.ts
+++ server/index.ts
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
         logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
       }
 
-      if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + "…";
-      }
+      // if (logLine.length > 80) {
+      //   logLine = logLine.slice(0, 79) + "…";
+      // }
 
       log(logLine);
     }
